a529f63db1d88c42ee0e06f0991b2547a81a4e6d
Henrique Bolzan <henrique@bolzan.com.br>
2020-06-30 17:02:08 -0300

    Project started

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+    entry: './src/index.js',
+    module: {
+        rules: [
+            {
+                test: /\.(js)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            }
+
+        ]
+    },
+    resolve: {
+        extensions: ['*', '.js']
+    },
+    output: {
+        path: __dirname + '/dist',
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
+    devServer: {
+        contentBase: './dist'
+    }
+};
