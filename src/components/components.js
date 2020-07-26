f2e6df0829e83e69eb1d7ce1b84dda84e068cd9b
Henrique Bolzan <henrique@bolzan.com.br>
2020-07-26 08:53:47 -0300

    Refactor components

---
+++ src/components/components.js
@@ -0,0 +1,32 @@
+import Icons from "uikit/dist/js/uikit-icons";
+import UIkit from "uikit";
+import pageDom from "./dom/main_page.js";
+import ComplexFormDom from "./dom/complex_form.js";
+import I18n from "./i18n/i18n.js";
+import HttpConnection from "./data_aware/http_connection.js";
+import { v4 as uuidv4 } from "uuid";
+
+UIkit.use(Icons);
+
+const host = () => "http://127.0.0.1:8000";
+
+const baseComponents = {
+    document,
+    UIkit,
+    HttpConnection,
+    uuidGen: uuidv4,
+    i18n: I18n("pt-BR"),
+    formsConnection: HttpConnection({
+        host: `${ host() }/api/query/persistent/complex-tables/?middleware=complex_forms&depth=1&id=`
+    }),
+};
+
+const components = Object.assign(
+    baseComponents,
+    {
+        pageDom: containerId => pageDom(baseComponents, containerId),
+        ComplexForm: (...params) => ComplexFormDom(baseComponents, ...params),
+    }
+);
+
+export default components;
--- src/index.js
+++ src/index.js
@@ -3,29 +3,10 @@ import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
 import "js-datepicker/dist/datepicker.min.css";
 import "./css/dashboard.css";
-import Icons from "uikit/dist/js/uikit-icons";
-import UIkit from "uikit";
+import components from "./components/components.js";
 
-import pageDom from "./components/dom/main_page.js";
-import ComplexFormDom from "./components/dom/complex_form.js";
-import I18n from "./components/i18n/i18n.js";
-import HttpConnection from "./components/data_aware/http_connection.js";
-import { v4 as uuidv4 } from "uuid";
-
-UIkit.use(Icons);
-
-const components = {
-    document,
-    uuidGen: uuidv4,
-    i18n: I18n("pt-BR"),
-    UIkit,
-    formsConnection: HttpConnection({
-        host: "http://127.0.0.1:8000/api/query/persistent/complex-tables/?middleware=complex_forms&depth=1&id="
-    }),
-};
-
-pageDom(components, "insertion-point").render("app-body");
-ComplexFormDom(components, "CAD_FORNECEDORES", "insertion-point").render();
+components.pageDom("insertion-point").render("app-body");
+components.ComplexForm("CAD_FORNECEDORES", "insertion-point").render();
 
 // let idField = dom.findFirst("attrs.name", "id");
 // idField.value(12345.67);
